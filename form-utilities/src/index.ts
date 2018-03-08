d17e40458ef437dc1295fce38995552926c0fdd5
Jannik Clausen <jc@braune-digital.com>
2018-03-08 15:41:52 +0100

    exporting all components exported from module

--- form-utilities/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './form-utilities.module';
-export * from './services'
---
+++ form-utilities/src/public_api.ts
@@ -0,0 +1,5 @@
+export * from './components';
+export * from './directives';
+export * from './interceptors';
+export * from './services';
+export * from './form-utilities.module';
