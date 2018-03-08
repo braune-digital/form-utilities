6953bdd73fd95c60d43b743789c6195a3d406c88
Jannik Clausen <jc@braune-digital.com>
2018-03-08 14:48:35 +0100

    removed old files

--- src/components/ng2-iq-select2/iq-select2/iq-select2-item.ts
+++
@@ -1,6 +0,0 @@
-export interface IqSelect2Item {
-    id: string;
-    text: string;
-    entity?: any;
-    isGroup?: boolean;
-}
--- src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './components';
-export * from './form-utilities.module';
-export * from './utils/HelperDate';
-export * from './directives/validate-on-blur.directive';
