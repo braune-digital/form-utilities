289d250a4be1090b7ea57c623b784163dc4ba85f
Felix Baltruschat <fb@braune-digital.com>
2018-11-30 15:53:23 +0100

    add class options to bdSelect directive

--- lib/src/directives/select/select.directive.ts
+++ lib/src/directives/select/select.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, Inject, Input, OnInit, Renderer2 } from '@angular/core';
+import { FormUtilitiesOptions } from '../../form-utilities.module';
 
 @Directive({
   selector: '[bdSelect]'
@@ -8,18 +9,18 @@ export class SelectDirective implements OnInit {
   @Input()
   label: string;
 
-  constructor(private renderer: Renderer2, private elementRef: ElementRef) {
+  constructor(@Inject('options') protected _options:FormUtilitiesOptions, private renderer: Renderer2, private elementRef: ElementRef) {
   }
 
   ngOnInit(): void {
     // Create a div with form-group
     const formGroupDiv = this.renderer.createElement('div');
-    this.renderer.addClass(formGroupDiv, 'form-group');
+    this.renderer.addClass(formGroupDiv, this._options.classFormGroup);
 
     if (this.label) {
       const label = this.renderer.createElement('label');
       const labelText = this.renderer.createText(this.label);
-      this.renderer.addClass(label, 'text-small');
+      this.renderer.addClass(label, this._options.classFormLabel);
       this.renderer.appendChild(label, labelText);
       this.renderer.appendChild(formGroupDiv, label);
     }
