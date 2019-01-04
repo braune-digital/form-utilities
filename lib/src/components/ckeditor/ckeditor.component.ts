ae81d2fbde393502a6884a023526f8f7d561aa81
Felix Baltruschat <fb@braune-digital.com>
2019-01-04 09:59:46 +0100

    remove ckeditor

--- lib/src/components/ckeditor/ckeditor.component.ts
+++ lib/src/components/ckeditor/ckeditor.component.ts
@@ -5,7 +5,9 @@ import { FormErrorService } from '../../services/form-error.service';
 import { CKEditorComponent } from 'ng2-ckeditor';
 import { FormUtilitiesOptions } from '../../form-utilities.module';
 
-
+/**
+ * @deprecated use rte component
+ */
 @Component({
   moduleId: module.id,
   providers: [{
@@ -16,29 +18,20 @@ import { FormUtilitiesOptions } from '../../form-utilities.module';
   selector: 'bd-ckeditor',
   templateUrl: './ckeditor.component.html'
 })
-export class CkeditorComponent extends FormInputComponent implements ControlValueAccessor {
+export class CkeditorComponent extends FormInputComponent{
 
   @ViewChild(DefaultValueAccessor)
   input: DefaultValueAccessor;
 
-  @ViewChild(CKEditorComponent)
-  editor: CKEditorComponent;
-
   @Input()
-  ckeditorConfig: {} = {
-    'skin': 'moono-lisa',
-    'toolbar': [
-      {
-        name: 'bd-form', items: ['Format', '-', 'Source', '-', 'Bold','Italic','Underline','StrikeThrough','-','NumberedList','BulletedList','-','JustifyLeft','JustifyCenter','JustifyRight','-','Link' ]
-      }
-    ]
-  };
+  ckeditorConfig: {} = {};
 
   @Input()
   inputClass: string;
 
   @Input()
   label: string;
+
   @Input()
   placeholder = '';
 
@@ -55,24 +48,12 @@ export class CkeditorComponent extends FormInputComponent implements ControlValu
     super(_options);
   }
 
-  onEditorChange(_value:any): void {
-    this.onChange(_value);
-  }
+  writeValue(writeValue){}
 
-  writeValue(value: any): void {
-    this.editor.value = value;
-  }
+  registerOnChange(fn: (_: any) => void): void {}
 
-  registerOnChange(fn: (_: any) => void): void {
-    this.onChange = fn;
-  }
+  registerOnTouched(fn: () => void): void {}
 
-  registerOnTouched(fn: () => void): void {
-    this.input.registerOnTouched(fn);
-  }
-
-  setDisabledState(isDisabled: boolean): void {
-    this.input.setDisabledState(isDisabled);
-  }
+  setDisabledState(isDisabled: boolean): void {}
 
 }
